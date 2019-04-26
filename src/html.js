00e602e92692f6b315cc31354867ff253118d101
Brandon Bolling <bollingb@aldenv188.allegheny.edu>
2019-04-26 01:40:35 -0400

    Messing with bootswatch.

--- src/html.js
+++ src/html.js
@@ -25,6 +25,12 @@ export default class HTML extends React.Component {
           />
           {this.props.postBodyComponents}
         </body>
+        <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h4 class="card-title">Primary card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+  </div>
       </html>
     );
   }
