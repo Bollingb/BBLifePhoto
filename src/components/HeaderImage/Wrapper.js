37102661bb8a2fda127738ec9c4ad74a508b0c5e
Brandon Bolling <bollingb@aldenv186.allegheny.edu>
2019-05-03 16:47:45 -0400

    Editing the maing image displayed.

--- src/components/HeaderImage/Wrapper.js
+++ src/components/HeaderImage/Wrapper.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   border: 5px solid #3598db;
   border-radius: 50%;
-  height: 150px;
+  height: 100px;
   margin: 0 auto 15px auto;
   overflow: hidden;
   width: 150px;
